12e65ad20db72e286c59fadc42480a23acfab120
Divyanshu Sharma <146127446+divyanshu7711@users.noreply.github.com>
2024-07-28 15:44:01 +0000

    Datatypes and conversion

---
+++ 3.js
@@ -0,0 +1,16 @@
+// conversion into various data types into one another 
+// 1.String
+// 2.Number
+// 3.Boolean
+// 4.Symbol
+// 5.BigInt
+
+let str1="558"
+console.log(typeof(str1));
+console.log(str1);
+let num1=0;
+console.log(typeof(num1));
+console.log(num1);
+let bool=Boolean(num1);
+// console.log(typeof(bool));
+console.log(bool);
