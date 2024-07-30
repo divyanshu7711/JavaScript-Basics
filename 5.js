b8598c995d52820e93cae5e16b674481d7150c53
Divyanshu Sharma <146127446+divyanshu7711@users.noreply.github.com>
2024-07-30 14:05:26 +0000

    Strings all methods

---
+++ 5.js
@@ -0,0 +1,28 @@
+// let name="Divyanshu";
+// console.log(name);
+// let roll=756
+
+// older version 
+// console.log("My name is" + name +" ." + "and My roll no is "+roll);
+
+// // newer version 
+// console.log(`MY name is ${name} . and My roll no is ${ roll}.`)
+
+
+let name=new String("Divyanshu is the most powerful    persson on the earth.");
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.charAt(5))
+// console.log(name.indexOf("u"));
+// console.log(name.trim());
+// console.log(name.slice(0,15));
+
+// console.log(name.slice());
+
+// agaer single hi rakhenge too iska mtlb h usi ke ye start samg legi aand end is not defined too use end tak lee jayengi .
+
+console.log(name.includes("D"));
+
+console.log(name.replace("nshu",""))
+console.log(name.endsWith("rth."));
+console.log(name.startsWith("Divyanshu"));
