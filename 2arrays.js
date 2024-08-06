b4cceb7b0d3068b55881019c0ed116bb2d4159ee
Divyanshu Sharma <146127446+divyanshu7711@users.noreply.github.com>
2024-08-06 22:12:37 +0000

    merger of 2 arrays

---
+++ 2arrays.js
@@ -0,0 +1,19 @@
+ let arr1 = [1,2,3,4,5,6];
+
+ let arr2= [5,6,7,8,9];
+//  arr1.push(arr2);
+//  console.log(arr1);
+
+//  console.log(arr1.concat(arr2));
+//  console.log(arr1);
+
+
+//                                  spread operator 
+
+// console.log([...arr1,...arr2])
+
+//  FLAT METHOD : IT IS USED WHEN THEIR ARE ARRAYS INSID EA ARRAY
+  
+let newarr=[5,7,9,[11,13,17,19],[1,2,5]];
+
+console.log(newarr.flat(3)); 
