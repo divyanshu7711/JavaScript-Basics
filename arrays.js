0d7d58835a07f8cc4d36d3ff7b73ab42b14b3f55
Divyanshu Sharma <146127446+divyanshu7711@users.noreply.github.com>
2024-08-06 21:29:35 +0000

    Single array

---
+++ arrays.js
@@ -0,0 +1,44 @@
+ // Declaration 
+// let arr=new Array(5,6,9,8);
+// console.log(arr);
+
+let arr=[0,1,2,3,4,5,6]
+console.log(arr);
+
+// console.log(arr.length);
+
+//                                      push and pop
+
+// arr.push(5,6)
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr);
+
+//                                      shift and unshift
+
+// arr.unshift(5,7);
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+
+
+//                                      includes , indexOf
+
+// console.log(arr.includes(3));
+// console.log(arr.indexOf(1));
+// console.log(arr[2]);
+
+//                                          joins
+
+// console.log(arr.join());
+
+
+//                                         SLice and splice
+// console.log(arr.slice(0,2));
+ 
+// console.log(arr);
+// console.log(arr.splice(0,2));
+
+// console.log(arr);
+
